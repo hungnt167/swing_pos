8c0bd4a8ab2938caa70ada1d5b991ccd571a9c28
hungnt <hungnt@smartosc.com>
2016-06-11 12:58:54 +0700

    build

---
+++ src/pos/models/Customer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pos.models;
+
+/**
+ *
+ * @author hungnt
+ */
+public class Customer extends Model{
+    
+}
